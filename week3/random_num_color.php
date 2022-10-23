a98c9a7cbc9e7b092feb68c7cb6d1db2046c0515
LOH HUI YI <huiyi199yi@gmail.com>
2022-10-23 23:36:27 +0800

    finish edit

--- week3/random_num_color.php
+++ week3/random_num_color.php
@@ -3,15 +3,16 @@
 <html>
 
 <head>
+    <title>Random Num Color</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <style>
-        .randColor1 {
-            color: red;
+        .randColor {
+            color: yellow;
             font-weight: bold;
-        }
-
-        .randColor2 {
-            color: blue;
-            font-weight: bold;
+            font-size: 100px;
         }
     </style>
 </head>
@@ -19,22 +20,37 @@
 <body>
 
     <?php
-    $rannum = rand(100, 200);
-    $rannum2 = rand(100, 200);
-
-    if ($rannum > $rannum2) {
-        echo "<span class=\"randColor1\">";
-        echo ($rannum . "<br>");
-        echo "</span> ";
-        echo ($rannum2 . "<br>");
-    } else {
-        echo ($rannum . "<br>");
-        echo "<span class=\"randColor2\">";
-        echo ($rannum2 . "<br>");
-        echo "</span> ";
-    }
-
+    $rand1 = rand(100, 200);
+    $rand2 = rand(100, 200);
     ?>
+
+    <div class="container justify-content-center">
+        <div class=" row justify-content-center text-center p-5 fs-3">
+            <div class="col-6 bg-dark text-light ">
+                <?php
+                if ($rand1 > $rand2) {
+                    echo "<span class=\"randColor\">";
+                    echo $rand1;
+                    echo "</span> ";
+                } else {
+                    echo $rand1;
+                }
+                ?>
+            </div>
+
+            <div class="col-6 bg-primary text-light">
+                <?php
+                if ($rand2 > $rand1) {
+                    echo "<span class=\"randColor\">";
+                    echo $rand2;
+                    echo "</span> ";
+                } else {
+                    echo $rand2;
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 
 </body>
 
--- week3/sum.php
+++ week3/sum.php
@@ -3,6 +3,11 @@
 <html>
 
 <head>
+    <title>Random Num Color</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     <style>
     </style>
 </head>
@@ -28,6 +33,28 @@
 
     ?>
 
+    <div class="container-fluid">
+        <div class="alert alert-danger" role="alert">
+            <?php
+            $sum = 0;
+
+            for ($x = 1; $x <= 100; $x++) {
+                $sum += $x;
+
+                if ($x % 2 == 0) {
+                    echo "<strong>" . $x . "</strong>";
+                    if ($x < 100) {
+                        echo "+";
+                    }
+                } else {
+                    echo $x, "+";
+                }
+            }
+
+            echo "=" . $sum;
+            ?>
+        </div>
+    </div>
 </body>
 
 </html>
