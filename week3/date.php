bf2ef3618ea5ac0901142cfc5d260e973f948d68
LOH HUI YI <huiyi199yi@gmail.com>
2022-10-24 23:13:21 +0800

    delete extra file

--- week3/date.php
+++ week3/date.php
@@ -14,67 +14,55 @@
 
 <body>
 
-    <!-- Example single danger button -->
-    <div class="btn-group">
-        <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Action
-        </button>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-            <li>
-                <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Separated link</a></li>
-        </ul>
+    <div class="row justify-content-center p-5">
+        <div class="col-2 ">
+            <select class="form-select form-select-lg mb-3 bg-info text-light " aria-label=".form-select-lg example">
+                <option selected>Day</option>
+
+                <?php
+                for ($i = 1; $i <= 31; $i++) {
+                ?>
+                    <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+
+                <?php
+                }
+                ?>
+
+            </select>
+        </div>
+
+        <div class="col-2 ">
+            <select class="form-select form-select-lg mb-3 bg-warning text-light " aria-label=".form-select-lg example">
+                <option selected>Month</option>
+
+                <?php
+                for ($m = 1; $m <= 12; $m++) {
+                ?>
+                    <option value="<?php echo $m; ?>"><?php echo $m; ?></option>
+
+                <?php
+                }
+                ?>
+
+            </select>
+        </div>
+
+        <div class="col-2 ">
+            <select class="form-select form-select-lg mb-3 bg-danger text-light " aria-label=".form-select-lg example">
+                <option selected>Year</option>
+
+                <?php
+                for ($y = 1900; $y <= 2022; $y++) {
+                ?>
+                    <option value="<?php echo $y; ?>"><?php echo $y; ?></option>
+
+                <?php
+                }
+                ?>
+
+            </select>
+        </div>
     </div>
-
-    <?php
-    function get_date()
-    {
-        $var = "";
-        for ($i = 1; $i < 31; $i++) {
-            $num_padded = sprintf("%02d", $i);
-            $var .= '<option value="' . $i . '">  ' . $num_padded . ' </option>';
-        }
-        return $var;
-    }
-    ?>
-    <select>
-        <?php echo get_date(); ?>
-    </select>
-
-    <?php
-    function get_month()
-    {
-        $var = "";
-        for ($m = 1; $m <= 12; $m++) {
-            $var .= '<option value="' . $m . '">' . date('F', mktime(0, 0, 0, $m, 1,      date('Y'))) . ' </option>';
-        }
-        return $var;
-    }
-    ?>
-    <select>
-        <?php echo get_month(); ?>
-    </select>
-
-    <?php
-    function get_year($start, $end)
-    {
-        $var = "";
-        while ($start <= $end) {
-            $var .= "<option value=" . $start . ">" . $start . "</option>";
-            $start++;
-        }
-        return $var;
-    }
-    ?>
-    <select>
-        <?php echo get_year(1900, 2022); ?>
-    </select>
-
-
 
 </body>
 
