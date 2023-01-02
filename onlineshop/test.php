cf4a935c89e634d1c038fa3b201d252ea4702e68
LOH HUI YI <huiyi199yi@gmail.com>
2023-01-03 03:58:18 +0800

    cleanup

--- onlineshop/test.php
+++
@@ -1,5 +0,0 @@
-<?php if (htmlspecialchars($promotion_price, ENT_QUOTES) == NULL ) {
-    echo "-";
-} else {
-    echo number_format((float)htmlspecialchars($promotion_price, ENT_QUOTES), 2, '.', '');
-}
